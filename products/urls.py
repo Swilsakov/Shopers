e78afe9dccf58653ee4c84724c4eeb83db68a05a
djangogirls <shkodaonebob@gmail.com>
2021-10-21 21:10:32 +0600

    shopers

---
+++ products/urls.py
@@ -0,0 +1,14 @@
+# from rest_framework import routers, urlpatterns
+# from rest_framework.routers import DefaultRouter
+# from products.views import *
+#
+# routers = DefaultRouter()
+#
+# routers.register('product', ProductViewSet)
+# routers.register('category', CategoryViewSet)
+# routers.register('cartproduct', CartProductViewSet)
+# routers.register('cart', CartViewSet)
+# routers.register('customer', CustomerViewSet)
+# routers.register('order', OrderViewSet)
+#
+# urlpatterns + routers
